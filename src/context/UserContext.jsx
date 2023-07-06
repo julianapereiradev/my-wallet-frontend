a8bc203ac831d8af67d61de08983b48c9906cce0
juliana <julianapereira.dev@gmail.com>
2023-07-06 16:39:49 -0300

    Adding possibility to acess route /cadastro when lsUser is null

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const UserContext = createContext()
 
@@ -8,14 +8,19 @@ export default function UserProvider({children}) {
     const lsUser = JSON.parse(localStorage.getItem("user"))
     const [user, setUser] = useState(lsUser !== null ? lsUser : {})
     const navigate = useNavigate()
+    const location = useLocation();
+
 
     useEffect(() => {
-        if(lsUser === null) {
-            navigate("/")
+        
+        if (lsUser === null && location.pathname !== '/cadastro') {
+          navigate('/');
+        } else if (lsUser === null && location.pathname === '/cadastro') {
+         console.log("Consegue mudar para a tela de cadastro escrevendo na URL")
         } else {
-            navigate("/home")
+          navigate('/home');
         }
-    },[])
+      }, []);
 
     console.log('lsUser aqui',lsUser)
     
